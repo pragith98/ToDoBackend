6a5ab3c15d996700d6ff77b462fb7d7799c5b33a
pragith98 <lakshanxp@hotmail.com>
2021-12-29 21:15:38 +0530

    create task update task function

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -17,10 +17,12 @@ class TaskController extends Controller
         return response()->json([$data],200);
     }
 
+
     public function getAllTask(){
         $task=Task::all();
         return response()->json(['allTask'=>$task],200);
     }
+
 
     public function getTask($id){
         $task=Task::find($id);
@@ -28,6 +30,18 @@ class TaskController extends Controller
             return response()->json(['msg'=>'task not found'],404);
         }else{
             return response()->json(['task'=>$task],200);
+        }
+    }
+
+
+    public function updateTask(Request $request,$id){
+        $task=Task::find($id);
+        if(!$task){
+            return response()->json(['msg'=>'task not found'],404);
+        }else{
+            $task->task=$request->input('task');
+            $task->save();
+            return response()->json(['updatedTask'=>$task],200);
         }
     }
 

1fb42e010fff799bebf4bb10bf09a421d16cc19f
pragith98 <lakshanxp@hotmail.com>
2021-12-29 17:28:25 +0530

    create store function

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    
+    public function store(Request $request){
+
+        $task = new Task;
+        $task->task=$request->task;
+        $task->status='new';
+        $task->save();
+
+        $data=Task::all();
+
+        return response()->json([$data]);
+
+    }
+
+
+
 }

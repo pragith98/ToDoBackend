101e4032cca5528b58a4cc6f693c9d4d717c1a27
pragith98 <lakshanxp@hotmail.com>
2021-12-29 21:04:19 +0530

    create get task function

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -32,6 +32,15 @@ class TaskController extends Controller
         
     }
 
+    public function getTask($id){
+        $task=Task::find($id);
+        if(!$task){
+            return response()->json(['msg'=>'task not found'],404);
+        }else{
+            return response()->json(['task'=>$task],200);
+        }
+    }
+
 
 
 }
